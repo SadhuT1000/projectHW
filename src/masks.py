c3081de9c9d66b372c93c403c63548e70c7c0bb6
Макс Боломожнов <maksbolomoznov@MacBook-Air-Maks.local>
2024-06-08 17:09:57 +0300

    init

---
+++ src/masks.py
@@ -0,0 +1,21 @@
+def get_mask_card_number(num: str) -> str:
+    """Функция принимающая номер карты и возвращающаяя ее маску"""
+    # num_str = str(num)
+    new = num[:4] + " " + num[4:6] + "**" + " " + "****" + " " + num[-4:]
+
+    return new
+
+
+# print(get_mask_card_number(7000792289606361))
+
+
+def get_mask_account(num2: str) -> str:
+    """Функция принимающая номер карты и возвращающаяя ее маску"""
+
+    # num2_str = str(num2)
+    new2 = "**" + num2[-4:]
+
+    return new2
+
+
+# print(get_mask_account(73654108430135874305))
---
+++ src/widget.py
@@ -0,0 +1,25 @@
+import masks
+
+
+def mask_account_card(nums: str) -> str:
+    """Функция возвращает строку с замаскированным номером карты/счета"""
+
+    if "Счет" in nums:
+        return masks.get_mask_account(nums)
+
+    else:
+        cards = masks.get_mask_card_number(nums[-16:])
+        new_card = nums.replace(nums[-16:], cards)
+        return new_card
+
+
+# print(mask_account_card('Maestro 1596837868705199'))
+
+
+def get_data(date: str) -> str:
+    """Функция преобразования даты"""
+
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+
+
+# print(get_data('2018-07-11T02:26:18.671407'))
